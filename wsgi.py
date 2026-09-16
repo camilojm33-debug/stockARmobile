6a468a1af8fb9182354a76527b084aeb45a4c637
camilojm33-debug <camilojm33@gmail.com>
2026-09-16 01:03:05 -0300

    fix: import Flask request context for MP QR tracing

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, request, url_for, jsonify, render_template
+from flask import flash, redirect, request, url_for, jsonify, render_template, g
 from flask_login import current_user, login_required
 from sqlalchemy import text
 
