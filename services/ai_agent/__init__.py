6cd3636318b17277767954f33e5a40955b071f56
camilojm33-debug <camilojm33@gmail.com>
2026-09-17 17:35:01 -0300

    feat: register public vendor checkout hardening

--- services/ai_agent/__init__.py
+++ services/ai_agent/__init__.py
@@ -2,5 +2,6 @@
 
 from .orchestrator import AgentOrchestrator
 from . import vendor_publication  # Registers stable Vendor IA routes on app context.
+from . import vendor_checkout_public  # Registers public checkout/order hardening routes.
 
 __all__ = ["AgentOrchestrator"]
